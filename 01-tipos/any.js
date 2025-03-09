99a9e578b0a7a6b5802c90e0be57501ed8794dd9
Jesús Ruvalcaba <Jruvalcabafsd@gmail.com>
2025-03-09 14:02:43 -0600

    Any

---
+++ 01-tipos/any.js
@@ -0,0 +1,14 @@
+"use strict";
+(() => {
+    let avengers = 123;
+    let exist;
+    let power;
+    avengers = 'Dr Strange';
+    // console.log(avengers.charAt(0));
+    console.log(avengers.charAt(0));
+    avengers = 150.2335;
+    // console.log(avengers.toFixed(2));
+    console.log(avengers.toFixed(2));
+    console.log(exist);
+    console.log(power);
+})();
