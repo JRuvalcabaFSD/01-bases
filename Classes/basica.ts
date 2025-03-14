6bb3c4b0ffce8e0883ebf73d1397f2ec80ef71b8
Jesús Ruvalcaba <Jruvalcabafsd@gmail.com>
2025-03-13 21:49:12 -0600

    Propiedades privadas y publicas

--- Classes/basica.ts
+++ Classes/basica.ts
@@ -1,8 +1,15 @@
 (() => {
 	class Avenger {
 		static avgAge: number = 35;
+		static getAvgAge() {
+			return this.name;
+		}
 
 		constructor(private name: string, private team: string, public realName?: string) {}
+
+		public bio() {
+			return `${this.name} (${this.team})`;
+		}
 	}
 
 	const antMan = new Avenger('Antman', 'Capitan', 'Scott Lang');
--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvgAge() {
+            return this.name;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
         }
     }
     Avenger.avgAge = 35;
