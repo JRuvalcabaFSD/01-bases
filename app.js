8a19ef004dbce68a354f480e8ed3542264f80da1
Jesús Ruvalcaba <Jruvalcabafsd@gmail.com>
2025-03-13 23:21:21 -0600

    Ejercicio

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const msg = 'Hola Mundo';
-let hero = {
-    name: 'Ironman',
-    age: 50,
-};
-hero.age = 50;
-console.log(hero.age + 1);
--- main.js
+++ main.js
@@ -1,4 +1,43 @@
 "use strict";
+var conducirBatimovil = function (auto) {
+    auto.encender = true;
+    auto.velocidadMaxima = 100;
+    auto.acelear();
+};
+var batimovil = {
+    encender: false,
+    velocidadMaxima: 0,
+    acelear: function () {
+        console.log('...... gogogo!!!');
+    },
+};
+var guason = {
+    reir: true,
+    comer: true,
+    llorar: false,
+};
+var reir = function (guason) {
+    if (guason.reir) {
+        console.log('JAJAJAJA');
+    }
+};
+var ciudadGotica = function (ciudadanos) {
+    return ciudadanos.length;
+};
+var Persona = (function () {
+    function Persona(nombre, edad, sexo, estadoCivil) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+    }
+    Persona.prototype.imprimirBio = function () {
+        console.log("Hola! soy ".concat(this.nombre, " tengo ").concat(this.edad, " a\u00F1os de tipo ").concat(this.sexo, " y estoy ").concat(this.estadoCivil));
+    };
+    return Persona;
+}());
+var jesus = new Persona('Jesus', 43, 'Masculino', 'Soltero');
+jesus.imprimirBio();
 (function () {
     var flash = {
         name: 'Barry Allen',
@@ -51,6 +90,12 @@
         getFullAddress: function (id) {
             return "Address: ".concat(this.address.id, ", ").concat(this.address.city, ", ").concat(this.address.zip);
         },
+    };
+})();
+(function () {
+    var addNumbersFunction;
+    addNumbersFunction = function (a, b) {
+        return 10;
     };
 })();
 //# sourceMappingURL=main.js.map
