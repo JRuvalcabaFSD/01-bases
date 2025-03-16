e07e7a8c1064cde6a2808dcce959db628385d9d3
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-16 02:56:34 -0600

    Basic TS-Node config

--- namespaces/validations.ts
+++
@@ -1,6 +0,0 @@
-namespace Validations {
-	export const validateText = (text: string): boolean => text.length > 3;
-	const validateDate = (myDate: Date): boolean => !isNaN(myDate.valueOf());
-}
-
-console.log(Validations.validateText('Jesus'));
