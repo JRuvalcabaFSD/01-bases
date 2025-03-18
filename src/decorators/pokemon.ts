2bbf8d570ec9fb361313ab38bfa1ce99aa413708
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-17 23:24:44 -0600

    Ejemplo de decorador

--- src/decorators/pokemon.ts
+++ src/decorators/pokemon.ts
@@ -2,7 +2,19 @@ function printtoConsole(constructor:Function) {
   console.log(constructor);  
 }
 
-@printtoConsole
+const printToConsoleConditional = (print:boolean=false):Function=>{
+  if (print) return printtoConsole
+  return ()=>{}
+  
+}
+
+const blockPrototype = function (constructor:Function) {
+  Object.seal(constructor)
+  Object.seal(constructor.prototype)
+}
+
+@blockPrototype
+@printToConsoleConditional(true)
 export class Pokemon {
   public publicApi:string = "https://pokeapi.co/api/v2/pokemon/"
   constructor(public name:string) {
