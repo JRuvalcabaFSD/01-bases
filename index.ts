434bcaf8d0441774dca2051c9073258b5c1c53da
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-18 00:35:53 -0600

    fin de seción

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get("/",(req,res)=>{
+  res.status(401).json({
+    "ok":false,
+    msg:"No hay token"
+  }  )
+})
+
+app.listen(port,()=>{
+  console.log(`Example app listerning at Http://localhost:${port}`); 
+})
