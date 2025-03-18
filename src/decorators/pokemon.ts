8c2efa11bdf8af024f6a0288fca50f9b7eb4e057
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-17 23:49:52 -0600

    decorador sobre metodos

--- src/decorators/pokemon.ts
+++ src/decorators/pokemon.ts
@@ -13,11 +13,27 @@ const blockPrototype = function (constructor:Function) {
   Object.seal(constructor.prototype)
 }
 
+function CheckValidPokemonID():Function {
+  return function(target:any,key:string,des:PropertyDescriptor){   
+    const original = des.value
+    des.value=(id:number)=>{
+      if (id<1 || id>800) return console.error("El id del pokemon debe estar entre el 1 y 800");
+      return original(id)
+      
+    }
+    
+  }
+}
+
 @blockPrototype
-@printToConsoleConditional(true)
+@printToConsoleConditional()
 export class Pokemon {
   public publicApi:string = "https://pokeapi.co/api/v2/pokemon/"
-  constructor(public name:string) {
+  constructor(public name:string) {}
+
+  @CheckValidPokemonID()
+  savePokemonToDB(id:number){
+    console.log(`Polemon save to DB ${id}`);
     
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import { Pokemon } from "./decorators/pokemon";
 
 const charmander = new Pokemon("Charmander");
 
-(Pokemon.prototype as any).customName="Pickachu"
 
-console.log(charmander);
+console.log();charmander.savePokemonToDB(3)
